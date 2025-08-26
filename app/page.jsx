5605db4cdc2cf9cf56ada5e7558fb58fa81a905c
emeka <emekaanodere595@gmail.com>
2025-08-26 13:52:40 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    <h1 className="bg-red-500 text-white p-5 text-2xl font-bold text-center">
-        THIS IS THE MAIN BRANCH
+    <h1 className="bg-blue-500 text-white p-5 text-2xl font-bold text-center">
+        THIS IS THE TEST BRANCH
     </h1>
   </div>
   );
